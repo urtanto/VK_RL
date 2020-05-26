89ada4ad272f3b3121a0bd8bd27bd81be9329347
Vlad-2005-lab <59056097+Vlad-2005-lab@users.noreply.github.com>
2020-05-26 23:43:43 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
-from vk_api.keyboard import VkKeyboard, VkKeyboardColor
-from smtplib import SMTP
+from vk_api.keyboard import VkKeyboard
 
-vk_session = vk_api.VkApi(token='1d6272d37a0861f7ab22be95d9cc244d3d8a39ba975dc3ada21eaeb4e2632f438daf1f4ad06d741632837')
+token = open('static/token.txt', mode='rt').read().split('\n')[0]
+vk_session = vk_api.VkApi(token=token)
 longpoll = VkBotLongPoll(vk_session, 195353132)
-keyboards = {'начать': [False, []]}
+keyboards = {'начать': [False,
+                        ["Привет", 'DEFAULT'],
+                        ["Кнопка", 'POSITIVE'],
+                        'Line',
+                        ["Закрыть", 'NEGATIVE']],
+             'закрыть': [False, ["Закрыть", 'NEGATIVE']]}
 
 
-def create_keyboard():
-    keyboard = vk_api.keyboard.VkKeyboard(one_time=False)
+def create_keyboard(text):
+    global keyboards
+    if keyboards[text][0]:
+        keyboard = vk_api.keyboard.VkKeyboard(one_time=True)
+    else:
+        keyboard = vk_api.keyboard.VkKeyboard(one_time=False)
     # False Если клавиатура должна оставаться откртой после нажатия на кнопку
     # True если она должна закрваться
     # DEFAULT = белый
     # POSITIVE = зелёный
     # NEGATIVE = красный
     # PRIMARY = синий
-    keyboard.add_button("Привет", color=vk_api.keyboard.VkKeyboardColor.DEFAULT)
-    keyboard.add_button("Кнопка", color=vk_api.keyboard.VkKeyboardColor.POSITIVE)
-    keyboard.add_line()  # Обозначает добавление новой строки
-    keyboard.add_button("Начать", color=vk_api.keyboard.VkKeyboardColor.NEGATIVE)
-    keyboard.add_line()
-    keyboard.add_button("Закрыть", color=vk_api.keyboard.VkKeyboardColor.PRIMARY)
+    for i in keyboards[text][1:]:
+        print(i)
+        if type(i) is list:
+            if i[1] == 'DEFAULT':
+                keyboard.add_button(i[0], color=vk_api.keyboard.VkKeyboardColor.DEFAULT)
+            elif i[1] == 'POSITIVE':
+                keyboard.add_button(i[0], color=vk_api.keyboard.VkKeyboardColor.POSITIVE)
+            elif i[1] == 'NEGATIVE':
+                keyboard.add_button(i[0], color=vk_api.keyboard.VkKeyboardColor.NEGATIVE)
+            elif i[1] == 'PRIMARY':
+                keyboard.add_button(i[0], color=vk_api.keyboard.VkKeyboardColor.PRIMARY)
+            else:
+                continue
+        elif i == 'Line':
+            keyboard.add_line()
+        else:
+            continue
+        print('+')
+    # keyboard.add_line()  # Обозначает добавление новой строки
+    # keyboard = vk_api.keyboard.VkKeyboard(one_time=False)
+    # keyboard.add_button(keyboards[text][1][0], color=vk_api.keyboard.VkKeyboardColor.DEFAULT)
     return keyboard.get_keyboard()
 
 
@@ -40,19 +64,23 @@ def main():
             print('Для меня от:', event.obj.message['from_id'])
             print('Текст:', event.obj.message['text'])
             response = event.message.text.casefold()
-            keyboard = create_keyboard()
-            empty_keyboard = create_empty_keyboard()
             vk = vk_session.get_api()
             if event.from_user:
                 if response.lower() == "привет":
-                    vk.messages.send(peer_id=event.obj.peer_id or event.message.peer_id, message="Приветики",
-                                     keyboard=keyboard, random_id=0)
+                    keyboard = create_keyboard('начать')
+                    vk.messages.send(user_id=event.obj.message['from_id'], message="Привет",
+                                     keyboard=keyboard, random_id=random.randint(0, 2 ** 64))
                 elif response.lower() == "начать":
-                    vk.messages.send(peer_id=event.obj.peer_id or event.message.peer_id, message="Начинаем",
-                                     keyboard=keyboard, random_id=0)
+                    keyboard = create_keyboard(response.lower())
+                    vk.messages.send(user_id=event.obj.message['from_id'], message="Начинаем",
+                                     keyboard=keyboard, random_id=random.randint(0, 2 ** 64))
                 elif response.lower() == "закрыть":
-                    vk.messages.send(peer_id=event.obj.peer_id or event.message.peer_id, message="Закрываем",
-                                     keyboard=empty_keyboard, random_id=0)
+                    empty_keyboard = create_empty_keyboard()
+                    vk.messages.send(user_id=event.obj.message['from_id'], message="Пока",
+                                     keyboard=empty_keyboard, random_id=random.randint(0, 2 ** 64))
+                else:
+                    keyboard = create_keyboard('начать')
+                    vk.messages.send(user_id=event.obj.message['from_id'], message="Начинаем", keyboard=keyboard, random_id=random.randint(0, 2 ** 64))
 
 
 if __name__ == '__main__':
