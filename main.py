7cadb4089ccfb7c8a62fd8417487a6f46ace5116
Vlad-2005-lab <59056097+Vlad-2005-lab@users.noreply.github.com>
2020-05-27 09:08:40 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
 from vk_api.keyboard import VkKeyboard
+from flask import request
+from data.users import User
+from data import db_session
 
+db_session.global_init("db/users.sqlite")
 token = open('static/token.txt', mode='rt').read().split('\n')[0]
 vk_session = vk_api.VkApi(token=token)
 longpoll = VkBotLongPoll(vk_session, 195353132)
-keyboards = {'начать': [False,
-                        ["Привет", 'DEFAULT'],
-                        ["Кнопка", 'POSITIVE'],
-                        'Line',
-                        ["Закрыть", 'NEGATIVE']],
-             'закрыть': [False, ["Закрыть", 'NEGATIVE']]}
+keyboards = {'login': [True,
+                       ["Войти", 'POSITIVE'],
+                       'Line',
+                       ["Зарегистрироваться", 'DEFAULT']],
+             'вход': [False, ["Зарегистрироваться", 'DEFAULT']],
+             'main_menu': [False, ['Обо мне', 'DEFAULT']]}
 
 
 def create_keyboard(text):
@@ -27,7 +31,6 @@ def create_keyboard(text):
     # NEGATIVE = красный
     # PRIMARY = синий
     for i in keyboards[text][1:]:
-        print(i)
         if type(i) is list:
             if i[1] == 'DEFAULT':
                 keyboard.add_button(i[0], color=vk_api.keyboard.VkKeyboardColor.DEFAULT)
@@ -43,7 +46,6 @@ def create_keyboard(text):
             keyboard.add_line()
         else:
             continue
-        print('+')
     # keyboard.add_line()  # Обозначает добавление новой строки
     # keyboard = vk_api.keyboard.VkKeyboard(one_time=False)
     # keyboard.add_button(keyboards[text][1][0], color=vk_api.keyboard.VkKeyboardColor.DEFAULT)
@@ -56,8 +58,51 @@ def create_empty_keyboard():
     # Эта функция используется для закрытия клавиатуры
 
 
-def main():
-    print('We start')
+def enter(id):
+    keyboard = create_keyboard('вход')
+    vk = vk_session.get_api()
+    vk.messages.send(user_id=id, message="Введите почту:", keyboard=keyboard, random_id=random.randint(0, 2 ** 64))
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            post = event.obj.message['text']
+            session = db_session.create_session()
+            if post in session.query(User).all():
+                user = session.query(User).filter(User.email == post).first()
+                break
+            elif post == 'Зарегистрироваться':
+                return register(id)
+            else:
+                vk.messages.send(user_id=id, message="Такой почты не зарегистрировано!\nВведите почту:",
+                                 keyboard=keyboard,
+                                 random_id=random.randint(0, 2 ** 64))
+    vk = vk_session.get_api()
+    vk.messages.send(user_id=id, message="Введите пароль:", keyboard=keyboard, random_id=random.randint(0, 2 ** 64))
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            password = event.obj.message['text']
+            if password == user.password:
+                return main(1, id)
+            elif password == 'Зарегистрироваться':
+                return register(id)
+            else:
+                vk.messages.send(user_id=id, message="Не правильный пароль!\nВведите пароль:", keyboard=keyboard,
+                                 random_id=random.randint(0, 2 ** 64))
+
+
+def register(id):
+    vk = vk_session.get_api()
+    vk.messages.send(user_id=id, message="Регистрация:",
+                     random_id=random.randint(0, 2 ** 64))
+
+
+def main(*func):
+    vk = vk_session.get_api()
+    if func[0] == 1:
+        keyboard = create_keyboard('main_menu')
+        vk.messages.send(user_id=func[1], message="Привет",
+                         keyboard=keyboard, random_id=random.randint(0, 2 ** 64))
+    elif func[0] == 0:
+        print('Robit')
     for event in longpoll.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
             print('Новое сообщение:')
@@ -66,22 +111,25 @@ def main():
             response = event.message.text.casefold()
             vk = vk_session.get_api()
             if event.from_user:
-                if response.lower() == "привет":
-                    keyboard = create_keyboard('начать')
+                if response.lower() in ["привет", "начать"]:
+                    keyboard = create_keyboard('login')
                     vk.messages.send(user_id=event.obj.message['from_id'], message="Привет",
-                                     keyboard=keyboard, random_id=random.randint(0, 2 ** 64))
-                elif response.lower() == "начать":
-                    keyboard = create_keyboard(response.lower())
-                    vk.messages.send(user_id=event.obj.message['from_id'], message="Начинаем",
                                      keyboard=keyboard, random_id=random.randint(0, 2 ** 64))
                 elif response.lower() == "закрыть":
                     empty_keyboard = create_empty_keyboard()
                     vk.messages.send(user_id=event.obj.message['from_id'], message="Пока",
                                      keyboard=empty_keyboard, random_id=random.randint(0, 2 ** 64))
+                elif response.lower() == "войти":
+                    return enter(event.obj.message['from_id'])
+                elif response.lower() == "зарегистрироваться":
+                    return register(event.obj.message['from_id'])
                 else:
-                    keyboard = create_keyboard('начать')
-                    vk.messages.send(user_id=event.obj.message['from_id'], message="Начинаем", keyboard=keyboard, random_id=random.randint(0, 2 ** 64))
+                    keyboard = create_keyboard('login')
+                    vk.messages.send(user_id=event.obj.message['from_id'], message="Привет", keyboard=keyboard,
+                                     random_id=random.randint(0, 2 ** 64))
+                if event.obj.message['from_id'] in [463771138, 0] and response.lower() == 'ADMIN':
+                    pass
 
 
 if __name__ == '__main__':
-    main()
+    main(0)
